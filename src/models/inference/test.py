3a03c339d66eb412d78400861e0314bd4be35452
kentamaeda1111 <kentamaeda1111@gmail.com>
2025-02-05 22:49:01 -0500

    Update63

--- src/models/inference/test.py
+++ src/models/inference/test.py
@@ -36,8 +36,8 @@ def check_and_install_requirements():
 check_and_install_requirements()
 
 # Global Settings
-MODEL_VERSION = "2gouki"  
-CHECKPOINT = "checkpoint-990"  
+MODEL_VERSION = "model3"  
+CHECKPOINT = "checkpoint-1980"  
 MAX_HISTORY = 5  
 BASE_MODEL = "google/gemma-2-2b-jpn-it"
 
